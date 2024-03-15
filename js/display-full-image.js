1e3a1800d19b051f96ea7cce1b001c892c6c46fb
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-03-15 16:00:11 +0300

    Реализует открытие и закрытие bigPicture

--- js/display-full-image.js
+++ js/display-full-image.js
@@ -1,12 +1,33 @@
-import {showOrHide} from './util.js';
+import {isEscapeKey} from './util.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
+const bigPictureCancel = bigPicture.querySelector('.big-picture__cancel');
+
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeBigPicture();
+  }
+};
+
+function openBigPicture () {
+  bigPicture.classList.remove('hidden');
+
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closeBigPicture () {
+  bigPicture.classList.add('hidden');
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+}
 
 const onPreviewClick = (evt) => {
   if (evt.target.matches('img.picture__img')) {
-    showOrHide(bigPicture);
+    openBigPicture();
   }
 };
 
 picturesContainer.addEventListener('click', onPreviewClick);
+bigPictureCancel.addEventListener('click', closeBigPicture);
