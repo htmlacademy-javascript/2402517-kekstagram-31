fa5444cd7717720d3519e7147740b9b1670bece6
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-03-30 17:01:37 +0300

    Отображает данные с сервера

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-import './display-previews.js';
+import { renderPreviewList } from './display-previews.js';
 import './display-full-image.js';
 import './display-upload-form.js';
 import './validate-upload-form.js';
 import './edit-scale-upload-image.js';
 import './edit-effects-upload-image.js';
+import { getData } from './api.js';
+import { savePhotos } from './photo-state.js';
+
+
+const photos = await getData();
+renderPreviewList(photos);
+savePhotos(photos);
