f201df03dbe6f425c885a3fd3625ed83b2a2becf
Aleksey Fedorov <138257434+fed-off@users.noreply.github.com>
2024-04-05 03:43:05 +0300

    Фиксит баг с открытием big-picture (таргет на комментах или лайках)

--- js/display-full-image.js
+++ js/display-full-image.js
@@ -49,10 +49,9 @@ const showComments = () => {
   bigPicture.querySelector('.social__comment-shown-count').textContent = commentShownCount;
 };
 
-function openBigPicture (evt) {
-  const previewId = Number(evt.target.parentNode.dataset.id);
-  const currentPhoto = getPhotoById(previewId);
-  const bigPictureImage = bigPicture.querySelector('.big-picture__img').querySelector('img');
+function openBigPicture (previewId) {
+  const currentPhoto = getPhotoById(+previewId);
+  const bigPictureImage = bigPicture.querySelector('.big-picture__img img');
 
   commentList.innerHTML = ''; // удаляет комментарии к фото, написанные в разметке, и с предыдущего открытия
   bigPictureImage.src = currentPhoto.url;
@@ -78,10 +77,16 @@ function closeBigPicture () {
 }
 
 const onPreviewClick = (evt) => {
-  if (evt.target.matches('img.picture__img')) {
-    evt.preventDefault();
-    openBigPicture(evt);
+  const targetPreview = evt.target.closest('.picture');
+
+  if(!targetPreview) {
+    return;
   }
+
+  evt.preventDefault();
+  const targetPreviewId = targetPreview.dataset.id;
+
+  openBigPicture(targetPreviewId);
 };
 
 picturesContainer.addEventListener('click', onPreviewClick);
